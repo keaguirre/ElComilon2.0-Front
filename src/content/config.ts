00550b5d2d34283406a474d1be78379781a6f363
keaguirre <ke.aguirre@duocuc.cl>
2024-10-10 20:29:43 -0300

    update Proyect, fix Carousel, still working on define collection from menu.json

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,6 @@
 import { defineCollection, z } from "astro:content"; //z -> zod schema
+import menuData from "./menu/menu.json"; // Importar el JSON';
+
 const menu = defineCollection({
     schema: z.object({
         title: z.string(),
@@ -6,6 +8,7 @@ const menu = defineCollection({
         price: z.number(),
         image: z.string(),
     }),
+    // data: menuData // Asignar los datos del JSON a la colección
 });
 
 export const collections = { menu };
